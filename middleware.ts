849353c840e081a2b10ad28aba2dc0e15d3cf9ac
Nafi Hermawan <nafihermawan@gmail.com>
2026-03-25 00:32:46 +0700

    Fix Supabase session refresh in middleware

--- middleware.ts
+++ middleware.ts
@@ -27,6 +27,7 @@ export async function middleware(request: NextRequest) {
       },
       setAll(cookiesToSet) {
         cookiesToSet.forEach(({ name, value, options }) => {
+          request.cookies.set(name, value);
           response.cookies.set(name, value, { ...options, secure: isProd });
         });
       },
